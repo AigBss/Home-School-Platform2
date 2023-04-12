1f53f8f2915a6ab3f3824be4b2825804a5a55dc0
wzf <2944009652@qq.com>
2023-04-12 14:21:45 +0800

    第一次提交

---
+++ src/main/java/com/example/homeschoolplatform/HomeSchoolPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeschoolplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSchoolPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeSchoolPlatformApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/homeschoolplatform/conf/MyBatisPlusConfig.java
@@ -0,0 +1,53 @@
+package com.example.homeschoolplatform.conf;
+//
+//import com.baomidou.mybatisplus.annotation.DbType;
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+//import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+//
+//@Configuration
+//@MapperScan("com.example.mapper")
+//public class MyBatisPlusConfig {
+//
+//    /**
+//     * 分页插件
+//     */
+//    @Bean
+//    public PaginationInterceptor paginationInterceptor() {
+//        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+////        paginationInterceptor.setDbType(DbType.MYSQL);
+//        return paginationInterceptor;
+//    }
+//
+//}
+
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@MapperScan("scan.your.mapper.package")
+public class MyBatisPlusConfig {
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+//    @Bean
+//    public ConfigurationCustomizer configurationCustomizer() {
+//        return configuration -> configuration.setUseDeprecatedExecutor(false);
+//    }
+}
