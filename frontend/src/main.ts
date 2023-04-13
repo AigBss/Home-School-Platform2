f7dd0dafd522a78610ef53a717469ddae2304f7f
wzf <2944009652@qq.com>
2023-04-13 17:12:21 +0800

    添加空格

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+// import Vue from 'vue'
+import router from './router/index'
+import vuex from  './store/index'
+import axios from 'axios'
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+import "lib-flexible/flexible.js"
+axios.defaults.baseURL="http://localhost:8080/"
+axios.defaults.withCredentials=true
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+// Vue.prototype.$echarts = echarts
+app.config.globalProperties.$http = axios
+app.use(router)
+app.use(vuex)
+app.use(ElementPlus)
+app.mount('#app')
