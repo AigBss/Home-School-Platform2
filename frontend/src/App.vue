dbb236cbb0e59f8d4029b8ae037361ee64bfc6cc
wzf <2944009652@qq.com>
2023-04-13 16:21:12 +0800

    添加空格

---
+++ frontend/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <el-menu mode="horizontal" @select="handleSelect">
+      <el-menu-item index="/login">登录</el-menu-item>
+      <!-- 在此处添加其他导航菜单项 -->
+    </el-menu>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleSelect(index) {
+      this.$router.push(index)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/main/java/com/example/homeschoolplatform/HomeSchoolPlatformApplication.java
+++ src/main/java/com/example/homeschoolplatform/HomeSchoolPlatformApplication.java
@@ -1,9 +1,11 @@
 package com.example.homeschoolplatform;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.example.homeschoolplatform")
 public class HomeSchoolPlatformApplication {
 
     public static void main(String[] args) {
